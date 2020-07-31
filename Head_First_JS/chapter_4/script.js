352ad035ac50d3913be3ada0e481b4d31bb0c02f
NidhitheCoder <nidhpv@gmail.com>
2020-07-31 14:34:27 +0530

    Head First JS chapter 4

--- Head_First_JS/chapter_4/script.js
+++ Head_First_JS/chapter_4/script.js
@@ -1,11 +1,11 @@
 
 
-//  let scores = [60,58,69,54,60,57];
-//  let solution  = scores[2];
-//  console.log("Solution 2 produced " + solution + " bubbles");
+ let scores = [60,58,69,54,60,57];
+ let solution  = scores[2];
+ console.log("Solution 2 produced " + solution + " bubbles");
 
-// let flavours = ["vannila","buttorscotch","lavender","chocolate","cookie dough"];
-// let flavourOfTheday = flavours[2];
-// console.log(flavourOfTheday);
-// flavours[2] = "Vannilla Chocolate Chip";
-// console.log(flavours[2]);
+let flavours = ["vannila","buttorscotch","lavender","chocolate","cookie dough"];
+let flavourOfTheday = flavours[2];
+console.log(flavourOfTheday);
+flavours[2] = "Vannilla Chocolate Chip";
+console.log(flavours[2]);
