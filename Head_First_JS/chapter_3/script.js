ec2994231e3bcfac29120c2826e41de91802d5a3
NidhitheCoder <nidhpv@gmail.com>
2020-07-31 10:41:45 +0530

    Head First JS chapter 3

--- Head_First_JS/chapter_3/script.js
+++ Head_First_JS/chapter_3/script.js
@@ -39,17 +39,60 @@
 // bark("spike",18);
 // //  Code with function end
 
+// function whatShallWeare(temp) {
+//     if(temp < 60) {
+// console.log("Wear a jacket");
+//     } else if(temp < 70) {
+//         console.log("Wear a Sweater");
+//     } else {
+//         console.log("Wear a T-shirt");
+//     }
+// }
 
-function whatShallWeare(temp) {
-    if(temp < 60) {
-console.log("Wear a jacket");
-    } else if(temp < 70) {
-        console.log("Wear a Sweater");
-    } else {
-        console.log("Wear a T-shirt");
-    }
+// whatShallWeare(50);
+// whatShallWeare(80);
+// whatShallWeare(60);
+
+// function calculateArea(r) {
+// let area;
+// if(r <= 0) {
+//     return 0;
+// } else {
+//     area = Math.PI * r * r;
+//     return area;
+// }
+// }
+
+// let radious = 5.2;
+// let theArea = calculateArea(radious);
+// console.log("The area is " + theArea);   // output is:- The area is 84.94866535306801
+let avatar = "generic";
+let skill = 1.0;
+let pointPerLevel = 1000;
+let UserPoints = 2008;
+
+function getAvatar(points) {
+  let level = points / pointPerLevel;
+  if (level === 0) {
+    return "Teddy Bear";
+  } else if (level === 1) {
+    return "Cat";
+  } else if (level >= 2) {
+    return "Gorilla";
+  }
 }
 
-whatShallWeare(50);
-whatShallWeare(80);
-whatShallWeare(60);
+function updatePoints(bonus,newPoints) {
+    let i = 0;
+    while(i < bonus) {
+        newPoints = newPoints + skill * bonus;
+        i = i + 1;
+    }
+    number = 5; // for test globel scope of a variable inside a function without let/var/const keyword .
+    return newPoints + UserPoints;
+}
+
+UserPoints = updatePoints(2,100);
+avatar = getAvatar(2112);
+getAvatar(2112)
+console.log(number);
--- Head_First_JS/read.md
+++ Head_First_JS/read.md
@@ -3,11 +3,39 @@
 ## Chapter 3
 
 #### Functions
+    Function is a reusable statement block that we can write inside a set of curly braces ({}). We can reuse a function over and over when you want.
+    Function have name and parameters(optional) and function name is used for involking function with ().
+    Function helps you avoid reusability of code and program become more readable.
 
-syntax :-
 
-    function functionName(parameter-1,parameter-2....parameter-N) {
-        // statement block
-    }
+    syntax :-
 
-    functionName(); // call or involk function
+        function functionName(parameter-1,parameter-2...,parameter-N) {   
+            //^ paramenters are optional
+            // statement block
+        }
+
+        functionName(arg-1,arg-2...,arg-N); // call or involk function with arguments(optional)
+
+
+    - Javascript passes arguments to a function using pass-by-value.
+    - there is no argument pass then parametes set to undefined.
+     
+        eg.
+        function makeTea(cups,tea) {
+            console.log(cups + " cups of " + tea);   // output :- 3 cups of undefined
+        }
+        maekTea(3);
+
+    - If we pass too many arguments JS ignores the extra arguments.
+    - Function return
+        funtion addNumbers (no1,no2){
+            let sum = no1 + no2;    // local variable
+            return sum;
+        }
+
+        let  result = addNumbers(12,12);   // global variable
+        console.log(result);   // output:-24
+
+
+    - variable scope of a variable inside the function is within the function.
