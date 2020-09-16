10c1d85b2f94cd32d00e3b53a2e6c12b6a344069
NidhitheCoder <nidhpv@gmail.com>
2020-09-16 23:17:13 +0530

    add new functions expression and declarations

--- Head_First_JS/chapter_11/script.js
+++ Head_First_JS/chapter_11/script.js
@@ -1,3 +1,4 @@
+
 // let board = document.getElementById("board");
 // // function handler(){
 // //    board.innerHTML = "Hellow welcome";
@@ -7,32 +8,65 @@
 
 // window.onload = function () {board.innerHTML = "Change text"}
 
-window.onload = init;
+// window.onload = init;
 
-let cookies = {
-    instructions : "Preheat oven to 350...",
-    bake : function(time){
-        console.log("Baking the cookies.");
-        setTimeout(done,time);
+// let cookies = {
+//     instructions : "Preheat oven to 350...",
+//     bake : function(time){
+//         console.log("Baking the cookies.");
+//         setTimeout(done,time);
+//     }
+// };
+
+// function init() {
+// let button = document.getElementById("bake");
+// button.onclick = handleButton;
+// };
+
+// function handleButton() {
+//     console.log("Time to bake the cookies.");
+//     cookies.bake(2500);
+// };
+
+// function done(){
+//     alert("Cookies are ready, take them out to cool");
+//     console.log("Cooling cookies");
+//     let cool = function(){
+//         alert("cookies are cool, time to eat.")
+//     }
+
+//     setTimeout(cool, 1000);
+
+//     setTimeout(() => {
+//         alert("Time to take cookies out of the oven");
+//     }, 10000);
+// }
+
+let migrating = true;
+
+if(migrating) {
+    quack(4);
+    console.log(fly);
+    fly(4);
+}
+
+let fly = function (num) {
+    let sound = "Flying";
+
+    function wingFlapper(){
+        console.log(sound);
+    }
+    for(let i=0;i<num;i++){
+       wingFlapper();
     }
 };
 
-function init() {
-let button = document.getElementById("bake");
-button.onclick = handleButton;
-};
-
-function handleButton() {
-    console.log("Time to bake the cookies.");
-    cookies.bake(2500);
-};
-
-function done(){
-    alert("Cookies are ready, take them out to cool");
-    console.log("Cooling cookies");
-    let cool = function(){
-        alert("cookies are cool, time to eat.")
+function quack(num){
+    let sound = "Quack";
+    function quacker(){
+        console.log(sound);
     }
-
-    setTimeout(cool, 1000);
+    for(let i=0;i<num;i++){
+       quacker();
+    }
 }
