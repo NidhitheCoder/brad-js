c0accd51e052f17ec10b53ec7de0373128eb6e3e
NidhitheCoder <nidhpv@gmail.com>
2020-09-16 07:30:24 +0530

    flight program complete

--- Head_First_JS/chapter_10/flight.js
+++ Head_First_JS/chapter_10/flight.js
@@ -1,10 +1,9 @@
-
 var passengers = [
-  { name: "smith", paid: true,ticket:"First Class" },
-  { name: "sara", paid: true, ticket:"Coach" },
-  { name: "john", paid: true, ticket:"First Class"},
-  { name: "alex", paid: false,ticket:"Coach" },
-  { name: "chris", paid: true,ticket:"First class"}
+  { name: "smith", paid: true, ticket: "First Class" },
+  { name: "sara", paid: true, ticket: "Coach" },
+  { name: "john", paid: true, ticket: "First Class" },
+  { name: "alex", paid: false, ticket: "Coach" },
+  { name: "chris", paid: true, ticket: "First Class" }
 ];
 
 function processPassengers(passengers, testFunction) {
@@ -24,25 +23,59 @@ function checkNotPaid(passenger) {
   return !passenger.paid;
 }
 
-let allCanFly = processPassengers(passengers, checkNoFlyList);
-if (!allCanFly) {
-  console.log(
-    "The plane can't take off: We have passenger in the non-fly-list."
-  );
-}
+// let allCanFly = processPassengers(passengers, checkNoFlyList);
+// if (!allCanFly) {
+//   console.log(
+//     "The plane can't take off: We have passenger in the non-fly-list."
+//   );
+// }
 
-let allPaid = processPassengers(passengers, checkNotPaid);
-if (!allPaid) {
-  console.log("The plane can take off: Not everyone has paid");
-}
+// let allPaid = processPassengers(passengers, checkNotPaid);
+// if (!allPaid) {
+//   console.log("The plane can take off: Not everyone has paid");
+// }
 
 function serveCustomer(passenger) {
-    if(passenger.ticket === "First Class") {
-        alert("Whould you like a cocktail or wine ?");
-    } else {
-        alert("Your choice is cola or water.");
-    }
+  //   if (passenger.ticket === "First Class") {
+  //     alert("Whould you like a cocktail or wine ?");
+  //   } else {
+  //     alert("Your choice is cola or water.");
+  //   }
+
+  //   createDrinkOrder(passenger);
+  //   createDrinkOrder(passenger);
+
+  let getDrinkOrderFunction = createDrinkOrder(passenger);
+  getDrinkOrderFunction();
+  // get dinner order function
+  getDrinkOrderFunction();
+  getDrinkOrderFunction();
+  // show movie
+  getDrinkOrderFunction();
+  // pick up trash
 }
+
+function createDrinkOrder(passenger) {
+  let orderFunction;
+  if (passenger.ticket === "First Class") {
+    orderFunction = function() {
+      console.log("Whould you like a cocktail or wine ?");
+    };
+  } else {
+    orderFunction = function() {
+      console.log("Your choice is cola or water.");
+    };
+  }
+  return orderFunction;
+}
+
+function servePassengers(passengers) {
+  for (let i = 0; i < passengers.length; i++) {
+    serveCustomer(passengers[i]);
+  }
+}
+
+servePassengers(passengers);
 
 // function fun(echo){
 //     console.log(echo);
