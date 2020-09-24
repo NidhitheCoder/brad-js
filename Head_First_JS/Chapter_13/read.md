d0cd7261c6b452c943f2623f9dd0d831ae4f71ba
NidhitheCoder <nidhpv@gmail.com>
2020-09-24 09:48:50 +0530

    pototype and examples

--- Head_First_JS/Chapter_13/read.md
+++ Head_First_JS/Chapter_13/read.md
@@ -4,3 +4,6 @@
 
 - Javascript objects can inherit properties and behaviour from other objects.
 - Javascript uses what is known as prototypal ineritance, and the object you are inheriting behaviour from is called the prototype.
+- Prototypes are dynamic.
+- syntax:
+    {constructorName}.prototype.{methodName/propertyName} = {value};
--- Head_First_JS/Chapter_13/script.js
+++ Head_First_JS/Chapter_13/script.js
@@ -1,25 +1,119 @@
-function Dog(name, weight, breed) {
-  this.species = "Canine";
+// function Dog(name, weight, breed) {
+//   this.species = "Canine";
+//   this.name = name;
+//   this.weight = weight;
+//   this.breed = breed;
+//   // this.bark = function() {
+//   //   if (this.weight > 10) {
+//   //     console.log("Woof woof");
+//   //   } else {
+//   //     console.log("yip yip");
+//   //   }
+//   // };
+// }
+
+// let fido = new Dog("fido", 8, "mixed");
+// let spoofy = new Dog("Spoofy", 8, "chihuahua");
+// spoofy.bark = function bark() {
+//   console.log("woof woof by spoofy");
+// };
+
+// Dog.prototype.run = function() {
+//     console.log("Running");
+// };
+
+// Dog.prototype.wag = function(){
+//   console.log("Wag");
+// };
+
+// Dog.prototype.bark = function(){
+//   if(this.weight > 10){
+//     console.log("Woof Woof");
+//   } else {
+//     console.log("Yip Yip");
+//   };
+// };
+
+// console.log(fido);
+// console.log(spoofy);
+// fido.bark();
+// spoofy.bark();
+// fido.run();
+// fido.wag();
+
+function Robot(name, year, owner) {
   this.name = name;
-  this.weight = weight;
-  this.breed = breed;
-  this.bark = function() {
-    if (this.weight > 10) {
-      console.log("Woof woof");
-    } else {
-      console.log("yip yip");
-    }
-  };
+  this.year = year;
+  this.owner = owner;
 }
 
-let fido = new Dog("fido", 8, "mixed");
-let spoofy = new Dog("Spoofy", 8, "chihuahua");
-spoofy.bark = function bark() {
-  console.log("woof woof by spoofy");
+Robot.prototype.maker = "ObjectsRUs";
+Robot.prototype.speaker = function() {
+  console.log("I am Speaking");
 };
 
-console.log(fido);
-console.log(spoofy);
+Robot.prototype.makeCoffee = function() {
+  console.log(this.name + " making coffee for you");
+};
 
-fido.bark();
-spoofy.bark();
+Robot.prototype.Blinklights = function() {
+  console.log(this.name + " blinking lights");
+};
+
+var robby = new Robot("Robby", 1956, "Dr.Morbius");
+var rosy = new Robot("Rossie", 1992, "Dr.John");
+
+rosy.cleanHouse = function() {
+  console.log("Rossie cleaning the house");
+};
+
+console.log(
+  robby.name +
+    " Was made by " +
+    robby.maker +
+    " in Year " +
+    robby.year +
+    " and owned by " +
+    robby.owner
+);
+
+console.log(
+  rosy.name +
+    " was made by " +
+    rosy.maker +
+    " in year " +
+    rosy.year +
+    " and owned by " +
+    rosy.owner
+);
+rosy.cleanHouse();
+
+// Dog.prototype.sit = function(){
+// console.log(this.name + " is now sitting.");
+// };
+
+// spoofy.sit();
+
+function Game() {
+  this.level = 0;
+}
+
+Game.prototype.play = function() {
+  // Player plays game here
+  this.level++;
+  console.log("Welcome to " + this.level);
+  this.unlock();
+};
+
+Game.prototype.unlock = function() {
+  console.log(this.name + " Is blasting you with laser beams");
+};
+
+let game = new Game();
+while (game.level < 42) {
+  game.play();
+};
+
+robby.play();
+
+
