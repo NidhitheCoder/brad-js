f57f6345d0066f16b3c7f2c169dfad5cb9c92a9c
NidhitheCoder <nidhpv@gmail.com>
2020-12-28 23:39:50 +0530

    array passing trough url parameter

--- dataPassingWithhref/name.js
+++ dataPassingWithhref/name.js
@@ -4,5 +4,5 @@ btn.addEventListener("click", () => {
   btn.style.backgroundColor = "orange";
   const name = document.getElementById("name").value;
   window.location.href =
-    "http://127.0.0.1:5500/dataPassingWithhref/test.html?name=" + name;
+    "http://127.0.0.1:5500/dataPassingWithhref/test.html?name=" + name +'&age=23&place=cochin';
 });
