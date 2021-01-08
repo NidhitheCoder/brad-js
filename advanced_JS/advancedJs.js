f20736ddf0cc1f7efa89d5d257c6c01d60714b8b
NidhitheCoder <nidhpv@gmail.com>
2021-01-08 17:39:35 +0530

    Advanced js  youtube tutorial completd

--- advanced_JS/advancedJs.js
+++ advanced_JS/advancedJs.js
@@ -1,72 +1,183 @@
-// Source Edureka youtube channel :-)
 
-// Advanced working with functions 
+// // Source Edureka youtube channel :-)
 
-// #1 Recursion
+// // Advanced working with functions 
 
-const pow = (x,n)=>{
-    return n === 1 ? x : x* pow(x,n-1);
-}
+// // #1 Recursion
 
-console.log(pow(2,4)); //16
-console.log(pow(14,1));
+// const pow = (x,n)=>{
+//     return n === 1 ? x : x* pow(x,n-1);
+// }
 
-// #2 Closure
-const add = (() => {
-    let counter = 0;
-    return () => counter+=1;
-})();
+// console.log(pow(2,4)); //16
+// console.log(pow(14,1));
 
-console.log(add());
-console.log(add());
-console.log(add());
+// // #2 Closure
+// const add = (() => {
+//     let counter = 0;
+//     return () => counter+=1;
+// })();
 
-// #3 The "New function"
-let sum = new Function('a','b', 'return a+b');
-console.log(sum(5,2));
+// console.log(add());
+// console.log(add());
+// console.log(add());
 
-// #4 Arrow Function
- hello = () => {
-      document.getElementById("demo").innerHTML += this;
-};
+// // #3 The "New function"
+// let sum = new Function('a','b', 'return a+b');
+// console.log(sum(5,2));
 
-// window object call the function
-window.addEventListener('load',hello);
-// Button object call function
-document.getElementById("btn").addEventListener('click',hello);
+// // #4 Arrow Function
+//  hello = () => {
+//       document.getElementById("demo").innerHTML += this;
+// };
 
-// #5 Rest and spread Operator 
-// rest operator
-const fun = (...input) =>{
-    let sum = 0;
-    for(let i of input){
-        sum +=i;
-    }
-    return sum;
-}
+// // window object call the function
+// window.addEventListener('load',hello);
+// // Button object call function
+// document.getElementById("btn").addEventListener('click',hello);
 
-console.log(fun(10,20,30,40));
-console.log(fun(1,2,4));
-console.log(fun(100,20,3));
+// // #5 Rest and spread Operator 
+// // rest operator
+// const fun = (...input) =>{
+//     let sum = 0;
+//     for(let i of input){
+//         sum +=i;
+//     }
+//     return sum;
+// }
 
-// spread operator
-let arr1 =[1,2,3];
-let arr2 = [ 5,6,7];
-let arr = [...arr1,...arr2];
-console.log(arr);
+// console.log(fun(10,20,30,40));
+// console.log(fun(1,2,4));
+// console.log(fun(100,20,3));
 
-// #6 Global object
-alert("Hello");
-// similer to 
-window.alert("Hello");
+// // spread operator
+// let arr1 =[1,2,3];
+// let arr2 = [ 5,6,7];
+// let arr = [...arr1,...arr2];
+// console.log(arr);
 
-// Function Objects
-const sayHi = () => {
-console.log("Hi");
-}
+// // #6 Global object
+// alert("Hello");
+// // similer to 
+// window.alert("Hello");
 
-console.log(sayHi.name);
-const f2 = (a,b) =>{};
-const many = (a,b,...more) => {};
-console.log(f2.length);
-console.log(many.length);
+// // #7 Function Objects
+// const sayHi = () => {
+// console.log("Hi");
+// }
+
+// console.log(sayHi.name);
+// const f2 = (a,b) =>{};
+// const many = (a,b,...more) => {};
+// console.log(f2.length);
+// console.log(many.length);
+
+// // #8 SetItemout
+// const sayHi = () => {
+//     console.log("Hiii With setTimeout");
+// };
+
+// setTimeout(sayHi, 4000);
+
+// // #9 setInterval
+// let timerId = setInterval(() => {
+//    console.log("tick tick");
+// }, 2000);
+
+// // After 10 second stop  
+// setTimeout(() => {
+//     clearInterval(timerId);
+//     console.log("Stop the interval")
+// }, 10000);
+
+// // # 10 Function Binding
+// let user = {
+//     firstName:"Smith"
+// };
+
+// function func(){
+//     console.log(this.firstName);
+// };
+
+// let funUser = func.bind(user);
+// funUser();
+
+// // #11 Javascript namespaces 
+// let num = 5;
+// let obj ={};
+// let str ="String is";
+// const sum = (x,y) =>{
+//     total =x + y;
+//     return total;
+// };
+
+// numr = sum(3,3);
+// console.log(numr);
+
+// // #12 Prototypes
+// //  12.1 prototypal inheritance
+
+// let pet = {
+//     eats:true
+// };
+
+// let dog ={
+//     jumbs:true
+// };
+
+// dog.__proto__ = pet; // (*);
+
+// console.log(dog);
+
+//  console.log(dog.eats);
+//  console.log(dog.jumbs);
+
+//  // 12.2 prototypes methods, objects without __proto__
+//  let pet ={
+//      eats:true
+//  };
+
+// //  Create new object with pet as a prototype
+// let dog = Object.create(pet);
+// console.log(dog.eats);
+
+// console.log(Object.getPrototypeOf(dog) === pet);
+
+// // Change the prototype of Dog
+// Object.setPrototypeOf(dog,{});
+// console.log(dog.eats) ;
+
+// // #13 try catch method
+//     try{
+//         console.log("try begin");
+//         let str = "funny";
+//         str = str[game];
+//         console.log("try ending " + str);
+//     } catch( err) {
+//         console.log("Error ");
+//         console.log(err);
+//     }
+
+// // #14 Modules in Javascript
+
+// let MODULE = (function(){
+// let module ={};
+// let privateVariable = 7;
+// function privateMethod(){
+//     //
+// }
+
+// module.moduleProperty =1;
+// module.moduleMethod = function () {
+//     // ...
+// }
+
+// return module;
+// }());
+
+// console.log(MODULE);
+
+// Chaining in Javascript
+account.number('112562354').setBalance(15000).applyCard(200);
+
+$("#myDiv").removeClass("Off").addClass("On").css("background:'red'")
--- advanced_JS/read.md
+++ advanced_JS/read.md
@@ -23,3 +23,55 @@
 ### Function objects 
 - In javascript functions are objects itself.
 - Properties :- name and length
+
+### setTimeout and setInterval
+- SetTimeout : Allows us to run a function once after the interval of time.
+    Syntax: 
+        let timerId = setTimeout(func|code,[delay],[arg1],[arg2],...);
+
+- SetInterval :  Allows us to run a function repeatedly. Starting after the   interval of time. Then repeating continuously at that interval.
+   Syntax:
+        let timerId = setInterval(func|code,[delay],[arg1],[arg2],...);
+
+### Function Binding
+- When passing object methods as callbacks, for instance to setTimeout, there's a known problems of "losing this". Functions provide a build in method bind that allows fixing this.
+
+- Syntax:
+    <!-- simple syntax -->
+    let boundFunc = func.bind(context);
+- This is not available in arrow functions.
+
+### Javascipt NameSpaces
+- Javascript does not support namespaces. But the namespaces are important  as they help reduce the number of identifiers for variables,objects and functions that are added to the global scope in your application. Javascript is flexible language and there are ways to work around this limitation and implement your own namespaces.
+
+### Prototypes
+- In javascript, Objects have a special hidden property  known as prototype which depicts either null or referances anouther object. Now, That object is called prototype.
+- Important features of prototype: prototypal inheritance and prototype methods , object without proto.
+
+- Methods for setup prototypes
+    1. Object.create(proto[,descriptors]);
+    2. Object.getPrototypeOf(obj);
+    3. Object.setPrototypeOf(Obj,Proto);
+
+### Error handling
+- For handling errors
+- Try catch Method
+    syntax:
+        try {
+            // code
+        }
+        catch(error) {
+            // error handling
+        }
+
+### Modules in javascript
+- A module is a self-contained piece of code  that group sematically-related variables and functions. Modules are not build in constructor in javascript. But the javascript module pattern provide a way to create modules that have well-defined  interfaces that are exposed clients of the module.
+
+### Chaining Javascript Method
+- Javascript allows you to involk mulitple methods on an object in a single expression.To involk multiple methods, We have chaining. Chaining is the process of stringing  the method calls together with dots between them.
+- syntax:
+        object.method1().method2().method3()...methodN();
+
+### Generators
+- Generators are special class of functions  that simplify the task of writing iterators. So, this functions produces  a sequence of results instead of a single value  and generates  a series of values.
+
